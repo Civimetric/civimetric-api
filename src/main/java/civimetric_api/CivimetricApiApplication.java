946450e3677af07e705a885179f933af473c0610
Civimetric <olivier.rousseaux@civimetric.com>
2018-07-19 15:37:04 +0200

    Init

---
+++ src/main/java/civimetric_api/CivimetricApiApplication.java
@@ -0,0 +1,12 @@
+package civimetric_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivimetricApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivimetricApiApplication.class, args);
+	}
+}
